be36e16ee98155fc35e5c2dff8c3d8bc9ee3c86f
Yexx <you@example.com>
2017-10-06 07:29:06 +0800

    1stt

--- WebTime.aspx.cs
+++ WebTime.aspx.cs
@@ -9,6 +9,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;  
 
 public partial class WebTime : System.Web.UI.Page
 {
@@ -19,4 +20,17 @@ public partial class WebTime : System.Web.UI.Page
         timeLabel.Text = string.Format( "{0:D2}:{1:D2}:{2:D2}",
            DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second );
      } // end method Page_Init
+     
+     
+     protected SqlConnection conn = new SqlConnection(ConfigurationManager.AppSettings["Conn"]);  
+    protected void Page_Load(object sender, EventArgs e)  
+    { 
+        string sql = "SELECT name, number, address, phone FROM student";
+        SqlDataAdapter cmd = new SqlDataAdapter(sql, conn);  
+        DataTable dt = new DataTable();  
+        cmd.Fill(dt);  
+        //dt.Rows.Count 
+        
+    } 
+     
   } // end class WebTime
